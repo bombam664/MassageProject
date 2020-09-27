95d2fb772bfecf618a5dc819e4c8b28f708de474
bombam664 <57762453+bombam664@users.noreply.github.com>
2020-09-27 16:32:29 +0700

    deleteall

--- User/404page.php
+++
@@ -1,7 +0,0 @@
-<section class="manual">
-    <div class="block-error background-error">
-        <img src="./assets_img/404error.svg" alt="error" class="img-error">
-        <p style="font-size:25px;color:#707070;">ไม่พบหน้าที่ค้นหา</p>
-        <a href="?module=main" class="btn-404">หน้าหลัก</a>
-    </div>
-</section>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['CustomerID']);
-unset($_SESSION['Name']);
-
-echo "<META HTTP-EQUIV='Refresh' CONTENT='1;URL=index.php'>";
-
-?>
