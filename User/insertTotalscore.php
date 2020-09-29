53f4fe0b8082c92e42c9beab1dda438125b22daa
bombam664 <57762453+bombam664@users.noreply.github.com>
2020-09-29 12:43:20 +0700

    Updatetotalscore

--- User/insertTotalscore.php
+++ User/insertTotalscore.php
@@ -5,13 +5,13 @@ session_start();
  $LessonID=$_GET['LessonID'];
  $Totalscore=$_GET['Totalscore'];
 
-// $sql="INSERT INTO totalscore(CustomerID,LessonID,Totalscore) VALUES('$CustomerID','$LessonID','$Totalscore')";
-// $query=mysqli_query($conn,$sql);
-//  if($query){
+$sql="INSERT INTO totalscore(CustomerID,LessonID,Totalscore) VALUES('$CustomerID','$LessonID','$Totalscore')";
+$query=mysqli_query($conn,$sql);
+ if($query){
 // 	echo "ok";
 	 echo "<META HTTP-EQUIV='Refresh' CONTENT='0;URL=?module=menu_quiz'>";
-//  }else{
-//  	echo "no ok";
-//  }
+ }else{
+ 	echo "no ok .$sql";
+ }
 mysqli_close($conn);
 ?>
